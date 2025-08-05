03bb3fdb5d5ffccf7b595fd18fcb738f278d85c0
runmasteryking <info.run.mastery@gmail.com>
2025-08-05 14:46:25 +0200

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,8 +1,14 @@
 // firebase-config.js
-import { initializeApp } from "firebase/app";
-import { getAuth, GoogleAuthProvider } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
+import {
+  getAuth,
+  GoogleAuthProvider
+} from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
+import {
+  getFirestore
+} from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
 
+// Din Firebase-konfiguration
 const firebaseConfig = {
   apiKey: "AIzaSyBx8seK9f-ZTV3JemDQ9sdTZkoiwSTvtqI",
   authDomain: "run-mastery-ai.firebaseapp.com",
@@ -12,8 +18,10 @@ const firebaseConfig = {
   appId: "1:599923677042:web:bc968a22483c7b3f916feb"
 };
 
+// Initiera Firebase
 const app = initializeApp(firebaseConfig);
 
+// Exportera autentisering, provider och databas
 export const auth = getAuth(app);
 export const provider = new GoogleAuthProvider();
 export const db = getFirestore(app);
